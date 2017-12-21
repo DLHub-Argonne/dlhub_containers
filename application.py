29a8f0920296d81e03e4781ded6186f0f0bfaec6
Ben Blaiszik <blaiszik@uchicago.edu>
2017-12-21 16:29:48 -0600

    simplified container

--- application.py
+++ application.py
@@ -4,57 +4,26 @@ import json
 import os
 import pickle
 
+from homerun.models import HRModel
+
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 from sklearn.externals import joblib 
 
-def train():
-	import numpy as np
-	from sklearn.datasets import load_iris
-	from sklearn.ensemble import RandomForestClassifier
+# Define where the models are
+model_file = "./model/iris.pkl"
 
-	from sklearn import linear_model, datasets
-
-	iris = load_iris()
-	X = iris.data[:, :]  # we only take the first two features.
-	y = iris.target
-
-	clf = RandomForestClassifier(max_depth=2, random_state=0)
-	clf.fit(X, y)
-	print(clf.predict(X))
-	joblib.dump(clf, 'filename.pkl')
-	return clf
-
-def load(model_file):
-	clf = joblib.load(model_file)
-	return clf
-
-def predict(clf, data):
-	X = np.array(data)
-	print(X)
-	return clf.predict(X)
-
-#print("=== training ===")
-#clf = train()
+# Instantiate a HRModel for sklearn type
+hr = HRModel(model_type="sklearn", 
+             model_file=model_file)
 
 @app.route("/", methods=["POST"])
 def slash_post():
 	body = request.get_json()
-	print(body)
-	print("=== Loading ===")
-	clf = load('./filename.pkl')
 
-	if body['batch'] is True:
-		print("=== batch predicting ===")
-		data_file='.data/data.json'
-		data = json.load(open(data_file))
-		res = predict(clf, data=data)
-		print(res)
-		return json.dumps({"output":res.tolist()})
-	elif body['input']:
+	if body['input']:
 		data = body['input']
-		res = predict(clf, data=data)
-		output = res.tolist()
-		body.update({"output":output})
+		res = hr.model.predict(np.array(data))
+		body.update({"output":res.tolist()})
 		return json.dumps(body)
