91b891d4878705b5b218204d0629b2f22edf7025
Ben Blaiszik <blaiszik@uchicago.edu>
2018-01-12 11:17:06 -0600

    Fixing odd Flask-keras bug. Remember to set FLASK_DEBUG=0 when serving a Keras model with TF backend through Flask

--- keras/application.py
+++ keras/application.py
@@ -4,7 +4,7 @@ import json
 import os
 import pickle
 
-from homerun.models import MLModel
+from home_run.models import HRModel
 
 from flask import Flask, request, jsonify
 app = Flask(__name__)
@@ -12,11 +12,11 @@ app = Flask(__name__)
 from sklearn.externals import joblib
 
 # Define where the models are
-model_file = "./model/my_model.h5"
- 
+model_file = "./model/test.h5"
+
 # Instantiate a HRModel for sklearn type
-ml = MLModel(model_type="keras",
-             model_file=model_file)
+ml = HRModel(model_type="keras",
+			 model_file=model_file)
 
 @app.route("/", methods=["POST"])
 def slash_post():
@@ -24,6 +24,26 @@ def slash_post():
 
 	if body['input']:
 		data = body['input']
+		print(np.array(data).shape)
 		res = ml.model.predict(np.array(data))
+
 		body.update({"output":res.tolist()})
 		return json.dumps(body)
+
+#Test route
+@app.route("/test", methods=["GET"])
+def slash_test():
+	from keras.datasets import mnist
+
+	body = {"input":[], "output":[]}
+	(x_train, y_train), (x_test, y_test) = mnist.load_data()
+	num_items = 2
+
+	data = {
+	    "batch":False,
+	    "input":x_test[0:num_items].reshape(num_items, 28, 28, 1).tolist()
+	}
+
+	res = ml.predict(np.array(data['input'][0:num_items]).reshape(num_items,28,28,1))
+	body.update({"output":res.tolist()})
+	return json.dumps(body)
