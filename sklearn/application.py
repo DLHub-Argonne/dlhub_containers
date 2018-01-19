f9cc92ae08c33ee8e8a179d681fdc0109c949de9
Logan Ward <ward.logan.t@gmail.com>
2018-01-19 11:34:25 -0600

    Overhauled sklearn container to work with new HR

--- sklearn/application.py
+++ sklearn/application.py
@@ -1,29 +1,23 @@
-
-import numpy as np
 import json
-import os
-import pickle
+from home_run.models import ScikitLearnModel
 
-from homerun.models import MLModel
-
-from flask import Flask, request, jsonify
+# Create the flask app
+from flask import Flask, request
 app = Flask(__name__)
 
 from sklearn.externals import joblib
 
-# Define where the models are
-model_file = "./model/iris.pkl"
+# Load in options form disk
+with open('options.json') as fp:
+    options = json.load(fp)
 
 # Instantiate a HRModel for sklearn type
-ml = MLModel(model_type="sklearn",
-             model_file=model_file)
+ml = ScikitLearnModel(**options)
 
 @app.route("/", methods=["POST"])
 def slash_post():
-	body = request.get_json()
-
-	if body['input']:
-		data = body['input']
-		res = ml.model.predict(np.array(data))
-		body.update({"output":res.tolist()})
-		return json.dumps(body)
+    body = request.get_json()
+    return json.dumps(ml.run(body))
+    
+if __name__ == "__main__":
+    app.run(debug=True)
