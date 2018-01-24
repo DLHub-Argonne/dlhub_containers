02d55a03a00232825e85516aaff4c3c63cd26a2b
Ben Blaiszik <blaiszik@uchicago.edu>
2018-01-24 16:01:36 -0600

    Adding support for CANDLE P1B2 model: Sparse Classifier Disease Type Prediction from Somatic SNPs. Model weights (.h5) are available on Petrel.

--- keras/client.py
+++ keras/client.py
@@ -1,16 +1,28 @@
 import requests
 from keras.datasets import mnist
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-num_items = 2
+DATASET = "candle"
 
-print(x_test.shape)
 
-data = [
- {'features':item.reshape(28,28,1).tolist()} for item in x_test[0:num_items]
-]
+if DATASET == "mnist": 
+	(x_train, y_train), (x_test, y_test) = mnist.load_data()
+	num_items = 2
 
+	data = [
+	 {'features':item.reshape(28,28,1).tolist()} for item in x_test[0:num_items]
+	]
+elif DATASET == "candle":
+	import pandas as pd
+
+	df = pd.read_csv('./data/x_test_candle.csv')
+
+	data = [
+	    {'features':row.values.tolist()} for index, row in df.iterrows()
+	]
+else:
+	data = []
 
 result = requests.post("http://0.0.0.0:5000/", json=data)
+res = result.json()
 print(result.status_code)
-print(result.json())
+print([item['prediction'] for item in res])
