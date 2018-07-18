4fb949be797f0011929bafc04e6a515312c16560
dep78 <dep78@cornell.edu>
2018-07-18 11:04:33 -0500

    Added Noop container to test dlhub functionality

---
+++ parsl-containers/noop/application.py
@@ -0,0 +1,11 @@
+# Intended to act as a noop and test functionality of dlhub
+
+def run(data):
+    return {}
+
+def test_run():
+    print("Running test run")
+    return {}
+
+if __name__ == '__main__':
+    test_run()
