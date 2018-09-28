c6313fca0fcdde4882dc779619fb5f6a07eb6b86
Logan Ward <ward.logan.t@gmail.com>
2018-09-28 13:09:20 -0500

    Added MNIST example

---
+++ mnist/README.md
@@ -0,0 +1,3 @@
+# MNIST Example
+Simple CNN trained on the MNIST benchmark problem.
+Example borrowed from [Keras documentation](https://github.com/keras-team/keras/blob/master/examples/mnist_cnn.py).
---
+++ mnist/describe_model.py
@@ -0,0 +1,26 @@
+from dlhub_toolbox.utils.schemas import validate_against_dlhub_schema
+from dlhub_toolbox.models.servables.keras import KerasModel
+import json
+
+
+# Describe the keras model
+model = KerasModel('model.h5', list(map(str, range(10))))
+
+#    Describe the model
+model.set_title("MNIST Digit Classifier")
+model.set_name("mnist")
+model.add_alternate_identifier("https://github.com/keras-team/keras/blob/master/examples/mnist_cnn.py",
+                               "URL")
+model.set_domains(["digit recognition"])
+
+#    Describe the outputs in more detail
+model.output['description'] = 'Probabilities of being 0-9'
+model.input['description'] = 'Image of a digit'
+
+# Print out the result
+# Sanity Check: Make sure it fits the schema
+metadata = model.to_dict()
+print(json.dumps(metadata, indent=2))
+validate_against_dlhub_schema(metadata, 'servable')
+with open('model_metadata.json', 'w') as fp:
+    json.dump(metadata, fp, indent=2)
