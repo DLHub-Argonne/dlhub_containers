e49ac6fa4e647218e5ace1dcd75e2996403e7867
Logan Ward <ward.logan.t@gmail.com>
2018-09-30 15:21:20 -0500

    Added DSIR model

---
+++ dsir/README.md
@@ -0,0 +1,4 @@
+# Deep-Learning Super-resolution Image Reconstruction
+
+Convolutional autoencoder [useful for super-resolution microscopy](https://github.com/leaxp/Deep-Learning-Super-Resolution-Image-Reconstruction-DSIR).
+It can be run against a directory of images, such as the ones available here: https://github.com/leaxp/Deep-Learning-Super-Resolution-Image-Reconstruction-DSIR
---
+++ dsir/describe_model.py
@@ -0,0 +1,34 @@
+from dlhub_toolbox.models.servables.python import PythonStaticMethodModel
+from dlhub_toolbox.utils.schemas import validate_against_dlhub_schema
+import json
+import os
+
+# Create a model that invokes the "run" function from the
+model = PythonStaticMethodModel('application', 'run')
+
+#  Describe the inputs and outputs
+model.set_inputs('list', 'Paths to all images in a dataset', item_type='string')
+model.set_outputs('ndarray', 'Accumulated result of decoding all the images', shape=[208, 208])
+
+#  Add provenance information
+model.set_title("Deep-Learning Super-resolution Image Reconstruction (DSIR)")
+model.set_name('dsir')
+model.set_authors(['Duarte, Alex'], ['The Institute of Photonic Sciences'])
+
+model.add_alternate_identifier("https://github.com/leaxp/Deep-Learning-Super-Resolution-Image-Reconstruction-DSIR", 'URL')
+
+#  Add requirements
+model.add_requirement('torch', 'detect')
+model.add_requirement('torchvision', 'detect')
+
+# Add app.py, which holds the noop function, to the list of associated files to be submitted
+model.add_file("app.py")
+model.add_file(os.path.join("Deep-Learning-Super-Resolution-Image-Reconstruction-DSIR",
+                            "model", "autoencoder_model.pt"))
+
+# Sanity Check: Make sure it fits the schema
+metadata = model.to_dict()
+print(json.dumps(metadata, indent=2))
+validate_against_dlhub_schema(metadata, 'servable')
+with open('model_metadata.json', 'w') as fp:
+    json.dump(metadata, fp, indent=2)
